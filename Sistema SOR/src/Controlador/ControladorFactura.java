eeeeab7f68604bb72641903f605e3ecac42a6b66
bboteo <raigeki_07@yahoo.es>
2022-10-06 14:37:47 -0600

    bau

---
+++ Sistema SOR/src/Controlador/ControladorFactura.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controlador;
+
+import Vista.FrmFactura;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+/**
+ *
+ * @author bboteo
+ */
+public class ControladorFactura implements ActionListener, WindowListener{
+
+    FrmFactura vFa = new FrmFactura();
+    
+    
+    public ControladorFactura(FrmFactura vFa) {
+        this.vFa = vFa;
+        
+    }
+
+    
+    
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void windowOpened(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+
+    }
+    
+}
